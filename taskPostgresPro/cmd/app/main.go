767f949ca8800586c115299f83c1134b4be0fab1
pythoniys <aurnysev24@gmail.com>
2024-05-07 22:47:40 +0300

    third commit

---
+++ taskPostgresPro/cmd/app/main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	//TODO:init config: cleanenv
+
+	//TODO:init logger: log/slog
+
+	//TODO: init storage: postgres
+
+	//TODO: init router: chi render (net/http)
+
+	//TODO: run server
+}
