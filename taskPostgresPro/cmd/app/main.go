744a2f020e1d9678854b4676d86bb21106a18e55
pythoniys <aurnysev24@gmail.com>
2024-05-10 11:03:40 +0300

    add storage directory

--- taskPostgresPro/cmd/app/main.go
+++ taskPostgresPro/cmd/app/main.go
@@ -1,13 +1,44 @@
 package main
 
-func main() {
-	//TODO:init config: cleanenv
+import (
+	config "github.com/pythoniys/taskPostgresPro/internal/config"
+	"log/slog"
+	"os"
+)
 
-	//TODO:init logger: log/slog
+const (
+	envLocal = "local"
+	envDev   = "dev"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting server", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
 
 	//TODO: init storage: postgres
 
-	//TODO: init router: chi render (net/http)
+	//TODO: init router: chi render3 (net/http)
 
 	//TODO: run server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	}
+	return log
+
 }
--- taskPostgresPro/internal/config/config.go
+++ taskPostgresPro/internal/config/config.go
@@ -1,1 +1,40 @@
 package config
+
+import (
+	"github.com/ilyakaznacheev/cleanenv"
+	"log"
+	"os"
+	"time"
+)
+
+type Config struct {
+	Env         string `yaml:"env" env-default:"local"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer  `yaml:"http_server"`
+}
+
+type HTTPServer struct {
+	Address     string        `yaml:"address" env-default:"localhost:8080"`
+	Timeout     time.Duration `yaml:"timeout"`
+	IdleTimeout time.Duration `yaml:"idle_timeout"`
+}
+
+func MustLoad() *Config {
+
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH environment variable not set")
+	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatal("CONFIG_PATH does not exist")
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalf("cannot read config: %s", err)
+	}
+
+	return &cfg
+}
